59986ea1f1fc235c6ea41bb1b09a4ff4f86ff002
Deenu Khan <dr.deenukhan001@gmail.com>
2023-07-18 06:44:26 +0000

    Further Changes

--- click-library/click-learning.py
+++ click-library/click-learning.py
@@ -1,1 +1,38 @@
 import click
+
+
+# A function becomes a Click command line tool by decorating it through click.command(). 
+# At its simplest, just decorating a function with this decorator will make it into a callable script:
+
+
+@click.command()
+@click.argument("argument")
+@click.option(
+    '--another_arg',
+    prompt="Another Arguement"
+)
+def main(argument, another_arg):
+    """
+    This is just check Function run it via giving one arguement
+    e.g python click-learning.py 'arguement name'
+    """
+    print(f"Click is working fine and this is the arguement passed : {argument} and {another_arg}")
+
+
+if __name__ == "__main__":
+    main()
+
+
+
+# import click
+
+# @click.command()
+# @click.option("--count", default=1, help="Number of greetings.")
+# @click.option("--name", prompt="Your name", help="The person to greet.")
+# def hello(count, name):
+#     """Simple program that greets NAME for a total of COUNT times."""
+#     for _ in range(count):
+#         click.echo(f"Hello, {name}!")
+
+# if __name__ == '__main__':
+#     hello()
---
+++ click-library/test/test_calculator.py
@@ -0,0 +1,11 @@
+def test_add(3, 5):
+    assert 3+5 == 8
+
+def test_subtract(5-3):
+    return 
+
+def test_multiply(3, 5):
+    return a*b
+
+def test_division(3, 5):
+    return a/b
